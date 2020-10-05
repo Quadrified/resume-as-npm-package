e03bdb1ad798361123ea0e091dd7f9cd15815f9c
DSMac <mohd.omerquadri@dsmartsys.in>
2020-10-05 23:15:27 +0530

    Add few more changes

--- index.js
+++ index.js
@@ -1,48 +1,51 @@
 #!/usr/bin/env node
-"use strict";
+'use strict';
 
-var inquirer = require("inquirer");
-var chalk = require("chalk");
+var inquirer = require('inquirer');
+var chalk = require('chalk');
 
-var response = chalk.bold.green;
+var response = chalk.bold.cyan;
 
-var resume = require("./resume.json");
+var resume = require('./resume.json');
 
 var resumePrompts = {
-  type: "list",
-  name: "resumeOptions",
-  message: "What do you want to know about me?",
-  choices: [...Object.keys(resume), "Exit"]
+  type: 'list',
+  name: 'resumeOptions',
+  message: 'What do you want to know about me?',
+  choices: [...Object.keys(resume), 'Exit'],
 };
 
 function main() {
-  console.log(" ");
-  console.log("Hello, World! 👋🏻 I'm Omer Quadri, and welcome to my resume (npm style)!");
-  console.log(" ");
+  console.log(' ');
+  console.log(' ');
   resumeHandler();
 }
 
 function resumeHandler() {
-  inquirer.prompt(resumePrompts).then(answer => {
-    if (answer.resumeOptions == "Exit") {
+  console.log(
+    "Hello, World! 👋🏻 I'm Omer Quadri, and welcome to my resume (npm style)!"
+  );
+  inquirer.prompt(resumePrompts).then((answer) => {
+    if (answer.resumeOptions == 'Exit') {
       return;
     }
     var option = answer.resumeOptions;
-    console.log(response("--------------------------------------"));
-    resume[`${option}`].forEach(info => {
-      console.log(response("|   => " + info));
+    console.log(response('--------------------------------------'));
+    resume[`${option}`].forEach((info) => {
+      console.log('\n' + response('|   => ' + info));
     });
-    console.log(response("--------------------------------------"));
+    console.log('\n' + response('--------------------------------------'));
     // console.log(resume[`${option}`]);
     inquirer
       .prompt({
-        type: "list",
-        name: "exitBack",
-        message: "Go back or Exit?",
-        choices: ["Back", "Exit"]
+        type: 'list',
+        name: 'exitBack',
+        message: 'Go back or Exit?',
+        choices: ['Back', 'Exit'],
       })
-      .then(choice => {
-        if (choice.exitBack == "Back") {
+      .then((choice) => {
+        if (choice.exitBack == 'Back') {
+          console.clear();
           resumeHandler();
         } else {
           return;
